8708b11869d932dd1bb1842d9db5a6d43aec8901
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-17 14:25:07 -0800

    Stress test GET route

---
+++ http_get.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 240,
+  duration: '600s',
+};
+
+export default function () {
+  const productId = Math.floor(1 + Math.random() * 10000000);
+  const res = http.get(`http://localhost:3001/reviews/all/${productId}`);
+  check(res, {
+    'status was 200': r => r.status === 200,
+  });
+  sleep(0.2);
+}
