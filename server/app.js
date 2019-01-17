2e938f29f4388f9a9ceea6effaaee43247a66cba
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-16 18:54:37 -0800

    Refactor server routes to use new database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../database/db.js');
 const Cors = require('cors');
+const db = require('../database/pg');
 
 const app = express();
 
@@ -11,54 +11,41 @@ app.use('/:productid', express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser());
 
-app.get('/reviews/all/:productid', (req, res) => {
-  const productId = req.params.productid;
-  const thisQuery = 'SELECT * FROM reviews WHERE product_id = ?';
-  db.query(thisQuery, [productId], (error, results) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(results);
-    }
-  });
+app.get('/reviews/all/:productId', async (req, res) => {
+  try {
+    const reviews = await db.getAllReviews(req.params.productId);
+    return res.send(reviews);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
 });
 
-app.get('/reviews/average/:productid', (req, res) => {
-  const productId = req.params.productid;
-  const thisQuery = 'SELECT * FROM reviews WHERE product_id = ?';
-  db.query(thisQuery, [productId], (error, results) => {
-    if (error) {
-      res.send(error);
-    } else {
-      let totalScore = 0;
-      for (let i = 0; i < results.length; i ++) {
-        totalScore += results[i].score
-      }
-      const average = totalScore / results.length;
-      const responseObject = {"averageScore": average, "totalReviews": results.length}
-      res.send(responseObject);
-    }
-  });
+app.get('/reviews/average/:productId', async (req, res) => {
+  try {
+    const reviews = await db.getAllReviews(req.params.productId);
+    const totalReviews = reviews.length;
+    const averageScore = reviews.reduce((sum, review) => sum + review.score, 0) / totalReviews;
+    return res.send({ averageScore, totalReviews });
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
 });
 
-app.post('/reviews/helpful/:reviewId', (req, res) => {
-  const thisId = req.params.reviewId;
-  const thisQuery = 'UPDATE reviews SET found_helpful = found_helpful + 1 WHERE id = ?';
-  console.log('post recieved');
-  db.query(thisQuery, [thisId], (err) => {
-    if (err) {
-      res.send(err);
-    } else {
-      const secondQuery = 'SELECT * FROM reviews WHERE id = ?';
-      db.query(secondQuery, [thisId], (error, results) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(results);
-        }
-      });
+app.post('/reviews/helpful/:reviewId', async (req, res) => {
+  try {
+    const { reviewId } = req.params;
+    const updatedReviewCount = await db.incrementFoundHelpful(reviewId);
+    if (updatedReviewCount === 0) {
+      return res.sendStatus(404);
     }
-  });
+    const updatedReview = await db.getReview(reviewId);
+    return res.send(updatedReview);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
 });
 
 module.exports = app;
