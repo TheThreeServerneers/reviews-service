89efe68d3f103a1849e1de8d712ae739eb4d1c58
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-19 12:07:37 -0800

    Preferentially make requests for the top products

--- http_get.js
+++ http_get.js
@@ -4,14 +4,18 @@ import { check, sleep } from 'k6';
 export const options = {
   vus: 240,
   duration: '300s',
+  rps: 2000,
 };
 
-const productId = Math.floor(1 + Math.random() * 10000000);
+const getProductId = (min, max) => Math.floor(min + Math.random() * (max - min));
+const topProductFraction = 0.7;
+const productId = (Math.random() <= topProductFraction)
+  ? getProductId(1, 1000)
+  : getProductId(1000, 10000000);
 
 export default function () {
   const res = http.get(`http://localhost:3001/reviews/all/${productId}`);
   check(res, {
     'status was 200': r => r.status === 200,
   });
-  sleep(0.2);
 }
