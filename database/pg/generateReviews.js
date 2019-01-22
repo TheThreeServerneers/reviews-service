fa3cd7f08f4284b4690608a43a21ef059a2a7e8a
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-21 18:33:03 -0800

    Generate data in chunks

--- database/pg/generateReviews.js
+++ database/pg/generateReviews.js
@@ -3,39 +3,43 @@ const faker = require('faker');
 const csvWriter = require('csv-write-stream');
 
 // const NUM_PRODUCTS = 10000000;
-const NUM_PRODUCTS = 1;
 const MAX_REVIEWS_PER_PRODUCT = 10;
 
-const writer = csvWriter();
-writer.pipe(fs.createWriteStream('reviews.csv'));
+const makeCSV = (filename, startIndex, NUM_PRODUCTS) => {
+  const writer = csvWriter();
+  writer.pipe(fs.createWriteStream(filename));
 
-let i = 1;
-let counter = 1;
+  let i = startIndex;
 
-const write = () => {
-  let ok = true;
+  const write = () => {
+    let ok = true;
 
-  while (i <= NUM_PRODUCTS && ok) {
-    const numReviews = faker.random.number({ max: MAX_REVIEWS_PER_PRODUCT, min: 0 });
-    for (let j = 0; j < numReviews; j += 1) {
-      ok = writer.write({
-        product_id: i,
-        username: faker.internet.userName(),
-        is_verified: faker.random.boolean(),
-        title: `${counter}${faker.random.words()}`,
-        review_text: faker.lorem.paragraph(),
-        score: faker.random.number({ max: 5, min: 1 }),
-        review_date: faker.date.between('2019-01-09', '2018-10-09').toISOString().split('T')[0],
-        found_helpful: faker.random.number(25),
-      });
-      counter += 1;
+    while (i <= NUM_PRODUCTS && ok) {
+      const numReviews = faker.random.number({ max: MAX_REVIEWS_PER_PRODUCT, min: 0 });
+      for (let j = 0; j < numReviews; j += 1) {
+        ok = writer.write({
+          product_id: i,
+          username: faker.internet.userName(),
+          is_verified: faker.random.boolean(),
+          title: faker.random.words(),
+          review_text: faker.lorem.paragraph(),
+          score: faker.random.number({ max: 5, min: 1 }),
+          review_date: faker.date.between('2019-01-09', '2018-10-09').toISOString().split('T')[0],
+          found_helpful: faker.random.number(25),
+        });
+      }
+      i += 1;
     }
-    i += 1;
-  }
 
-  if (i <= NUM_PRODUCTS) {
-    writer.once('drain', write);
-  }
+    if (i <= NUM_PRODUCTS) {
+      writer.once('drain', write);
+    }
+  };
+
+  write();
 };
 
-write();
+makeCSV('reviews1.csv', 1, 2500000);
+// makeCSV('reviews2.csv', 2500001, 5000000);
+// makeCSV('reviews3.csv', 5000001, 7500000);
+// makeCSV('reviews4.csv', 7500001, 10000000);
